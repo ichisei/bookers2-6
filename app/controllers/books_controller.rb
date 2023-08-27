1b6a9066e04d16b90a95fd9035de14d22a83e189
ichisei <132669382+ichisei@users.noreply.github.com>
2023-08-27 04:04:16 +0000

    [Finish7a-2]bookers2-6

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,13 +9,16 @@ class BooksController < ApplicationController
 
   def index
     # @books = Book.all
+    to  = Time.current.at_end_of_day
+    from  = (to - 6.day).at_beginning_of_day
+    @books = Book.all.sort {|a,b|
+      b.favorites.where(created_at: from...to).size <=>
+      a.favorites.where(created_at: from...to).size
+    }
     @book = Book.new
-    to = Time.current.at_end_of_day
-    from = (to - 6.day).at_beginning_of_day
-    @books = Book.includes(:favorited_users).
-      sort_by {|x|
-        x.favorited_users.includes(:favorites).where(created_at: from...to).size
-      }.reverse
+      # sort_by {|x|
+      #   x.favorited_users.includes(:favorites).where(created_at: from...to).size
+      # }.reverse
   end
 
   def create
